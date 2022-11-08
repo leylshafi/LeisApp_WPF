fd6e985074f6e392707214a01b731e6f14d9c1fd
Isa Memmedli <i.a.memmedli@gmail.com>
2022-11-09 00:14:40 +0400

    Left and Right side and tweet have done

--- Source/Views/UserControls/TweetControl.xaml.cs
+++ Source/Views/UserControls/TweetControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Source.UserControls
+namespace Source.Views.UserControls
 {
     /// <summary>
     /// Interaction logic for TweetControl.xaml
