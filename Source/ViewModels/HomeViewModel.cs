0e424957a6a429b9bda2df08a3eaebda679e9418
Leyla Shafiyeva <llshafi03@gmail.com>
2022-11-12 17:17:16 +0400

    Bind

--- Source/ViewModels/HomeViewModel.cs
+++ Source/ViewModels/HomeViewModel.cs
@@ -10,60 +10,22 @@ using System.Windows.Input;
 using System.Windows;
 using Source.Models;
 using Source.Repositories;
+using Source.Repositories.Contexts;
 
 namespace Source.ViewModels;
 
 class HomeViewModel:ViewModelBase
 {
-    //private readonly NavigationStore _navigationStore;
-    //public ObservableCollection<Tweet> Tweets { get; set; }
+    public ObservableCollection<Tweet>? Tweets { get; set; }
 
+    public HomeViewModel()
+    {
+        Tweets = new();
+        for (int i = 0; i < FakeDbContext.Tweets.Count; i++)
+        {
+            Tweets.Add(FakeDbContext.Tweets[i]);
+        }
+    }
 
-    //public HomeViewModel(FakeTweetRepository tweetRepository, NavigationStore navigationStore)
-    //{
-    //    Tweets = tweetRepository;
-    //}
-
-
-    //bool CanExecuteCommand(object? parametr) => SelectedCar is not null;
-
-    //void ExecuteShowCommand(object? parametr) => MessageBox.Show(SelectedCar?.Make);
-
-    //void ExecuteEditCommand(object? parametr)
-    //{
-    //    EditViewModel editViewModel = new EditViewModel(SelectedCar, _navigationStore, _carRepository);
-
-    //    EditView editView = new();
-    //    _navigationStore.CurrentViewModel = editViewModel;
-    //}
-
-    //void ExecuteAddCommand(object? parametr)
-    //{
-    //    AddViewModel addViewModel = new(_carRepository, _navigationStore);
-
-    //    AddView addView = new();
-    //    addView.DataContext = addViewModel;
-
-    //    _navigationStore.CurrentViewModel = addViewModel;
-    //}
-
-    //void ExecuteDeleteCommand(object? parametr) => Cars.Remove(SelectedCar!);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    ////////////Fake Tweets
-   
 
 }
