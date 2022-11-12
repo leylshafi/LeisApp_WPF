5b01f52758e881ec0308f0fe52c3a747f2da70f3
Isa Memmedli <i.a.memmedli@gmail.com>
2022-11-12 14:24:08 +0400

    Added early Tweet and FakeTweetRepository

---
+++ Source/Models/Tweet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source.Models
+{
+    class Tweet
+    {
+        public Guid Id { get; set; }
+        // string will has been User
+        public string User { get; set; }
+        public string Content { get; set; }
+    }
+}
