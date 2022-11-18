a0935cb46cf2f059729088b78d240a0281e0253a
Leyla Shafiyeva <llshafi03@gmail.com>
2022-11-18 15:49:30 +0400

    add sign in page

---
+++ Source/Commands/NavSignupCommand.cs
@@ -0,0 +1,25 @@
+﻿using Source.Stores;
+using Source.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source.Commands
+{
+    class NavSignUp : CommandBase
+    {
+        private readonly NavigationStore _navigationStore;
+
+        public NavSignUp(NavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.CurrentViewModel = new SignUpViewModel();
+        }
+    }
+}
---
+++ Source/ViewModels/MainViewSignup.cs
@@ -0,0 +1,33 @@
+﻿using Source.Commands;
+using Source.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Source.ViewModels
+{
+    class MainViewSignup : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        public ViewModelBase CurrentViewModel => _navigationStore.CurrentViewModel;
+
+        //public ICommand GoSigninCommand { get; }
+
+        public MainViewSignup(NavigationStore navigationStore)
+        {
+            //GoSigninCommand = new NavProfileCommand(navigationStore);
+
+            _navigationStore = navigationStore;
+            _navigationStore.CurrentViewModel = new SignUpViewModel();
+            _navigationStore.CurrentViewModelChanged += _navigationStore_CurrentViewModelChanged;
+        }
+
+        private void _navigationStore_CurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
+    }
+}
