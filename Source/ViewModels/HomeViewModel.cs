4120ec133420d9dae06442a40d2f538449e487e2
Isa Memmedli <i.a.memmedli@gmail.com>
2022-11-22 21:04:47 +0400

    fixed some error

--- Source/ViewModels/HomeViewModel.cs
+++ Source/ViewModels/HomeViewModel.cs
@@ -13,105 +13,105 @@ using System.Linq;
 using System;
 using System.Windows;
 
-namespace Source.ViewModels;
-
-class HomeViewModel : ViewModelBase
+namespace Source.ViewModels
 {
-    private string _content;
 
-    public string Content
+    class HomeViewModel : ViewModelBase
     {
-        get => _content;
-        set
+        private string _content;
+
+        public string Content
         {
-            _content = value;
-            OnPropertyChanged(nameof(Content));
-        }
-    }
-    public static ObservableCollection<Tweet>? Tweets { get; set; }
-    public ICommand SetImageCommand { get; set; }
-    public ICommand AddCommand { get; set; }
-
-    public User? User { get; set; }
-    public List<Tweet>? UserTweets { get; set; }
-
-
-
-    private Tweet? _tweet;
-
-    public Tweet? SelectedTweet
-    {
-        get { return _tweet; }
-        set
-        {
-            _tweet = value;
-            OnPropertyChanged(nameof(SelectedTweet));
-        }
-    }
-
-    private string? _imagePath;
-
-    public string? ImagePath
-    {
-        get { return _imagePath; }
-        set
-        {
-            _imagePath = value;
-            OnPropertyChanged(nameof(ImagePath));
-        }
-    }
-
-    public HomeViewModel()
-    {
-        User = MainViewModel.User;
-        UserTweets = MainViewModel.UserTweets;
-        if (User != null)
-        {
-            Tweets = new();
-            for (int i = 0; i < UserTweets?.Count; i++)
+            get => _content;
+            set
             {
-                Tweets.Add(UserTweets[i]);
-                SelectedTweet = Tweets[i];
+                _content = value;
+                OnPropertyChanged(nameof(Content));
             }
-
-            ImagePath = string.Empty;
         }
-        AddCommand = new RelayCommand(AddExecuteCommand, AddCanExecuteCommand);
-    }
+        public static ObservableCollection<Tweet>? Tweets { get; set; }
+        public ICommand SetImageCommand { get; set; }
+        public ICommand AddCommand { get; set; }
 
-    private bool AddCanExecuteCommand(object? obj)
-    {
-        return true;
-    }
+        public User? User { get; set; }
+        public List<Tweet>? UserTweets { get; set; }
 
-    private void AddExecuteCommand(object? obj)
-    {
-        Tweets?.Insert(0,new Tweet()
+
+
+        private Tweet? _tweet;
+
+        public Tweet? SelectedTweet
         {
-            Content= Content,
-            Created=DateTime.Now,
-            User=User,
-        });
-        Content=String.Empty;
+            get { return _tweet; }
+            set
+            {
+                _tweet = value;
+                OnPropertyChanged(nameof(SelectedTweet));
+            }
+        }
 
-        });
-    }
+        private string? _imagePath;
+
+        public string? ImagePath
+        {
+            get { return _imagePath; }
+            set
+            {
+                _imagePath = value;
+                OnPropertyChanged(nameof(ImagePath));
+            }
+        }
+
+        public HomeViewModel()
+        {
+            User = MainViewModel.User;
+            UserTweets = MainViewModel.UserTweets;
+            if (User != null)
+            {
+                Tweets = new();
+                for (int i = 0; i < UserTweets?.Count; i++)
+                {
+                    Tweets.Add(UserTweets[i]);
+                    SelectedTweet = Tweets[i];
+                }
+
+                ImagePath = string.Empty;
+            }
+            AddCommand = new RelayCommand(AddExecuteCommand, AddCanExecuteCommand);
+        }
+
+        private bool AddCanExecuteCommand(object? obj)
+        {
+            return true;
+        }
+
+        private void AddExecuteCommand(object? obj)
+        {
+            Tweets?.Insert(0, new Tweet()
+            {
+                Content = Content,
+                Created = DateTime.Now,
+                User = User,
+            });
+            Content = String.Empty;
+
+        }
 
     bool CanExecuteCommand(object? parametr) => true;
 
-    void ExecuteShowCommand(object? parametr)
-    {
-
-        OpenFileDialog openFileDialog1 = new OpenFileDialog();
-        openFileDialog1.Filter = "img files (*.img)|*.png|All files (*.*)|*.*";
-
-        if (openFileDialog1.ShowDialog() == true)
+        void ExecuteShowCommand(object? parametr)
         {
 
-            string filePath = System.IO.Path.GetFullPath(openFileDialog1.FileName);
-            StreamReader sr = new StreamReader(filePath);
-            ImagePath = filePath;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "img files (*.img)|*.png|All files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == true)
+            {
+
+                string filePath = System.IO.Path.GetFullPath(openFileDialog1.FileName);
+                StreamReader sr = new StreamReader(filePath);
+                ImagePath = filePath;
+            }
         }
     }
-
 }
